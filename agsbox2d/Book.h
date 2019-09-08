3fce2b4de41ad67ae8877b6c0bf5e9df392274c7
ericoporto <ericoporto2008@gmail.com>
2019-09-08 13:54:26 -0300

    adds license on top of files fix #12

--- agsbox2d/Book.h
+++ agsbox2d/Book.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2019  Érico Vieira Porto
+ *
+ * This program is free software. You can use and redistribute it
+ *  under the terms and conditions of the zlib-license (see LICENCE).
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
 #pragma once
 
 #ifndef AGSBOX2D_BOOK_H
--- agsbox2d/agsbox2d.cpp
+++ agsbox2d/agsbox2d.cpp
@@ -1,4 +1,12 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
+// agsbox2d.cpp : Defines the entry point for the DLL application.
+/*
+ * Copyright (C) 2019  Érico Vieira Porto
+ *
+ * This program is free software. You can use and redistribute it
+ *  under the terms and conditions of the zlib-license (see LICENCE).
+ *
+ * SPDX-License-Identifier: Zlib
+ */
 
 #pragma region Defines_and_Includes
 
