d1ea900f9855973c6f8e49a307048587d1de14f8
ericoporto <ericoporto2008@gmail.com>
2019-11-26 19:14:01 -0300

    fix comments

--- agsbox2d/AgsFixtureArray.h
+++ agsbox2d/AgsFixtureArray.h
@@ -1,9 +1,13 @@
-#pragma once
 /*
-  code based on  AGS Array plugin -- copyright (c) 2011 Ferry Timmers.  All rights reserved.
-  the AGS Array plugin is provided under the Artistic License 2.0
-*/
+ * Copyright (C) 2019  Érico Vieira Porto
+ *
+ * This program is free software. You can use and redistribute it
+ *  under the terms and conditions of the zlib-license (see LICENCE).
+ *
+ * SPDX-License-Identifier: Zlib
+ */
 
+#pragma once
 
 #ifndef _AGS_FIXTURE_ARRAY_H
 #define _AGS_FIXTURE_ARRAY_H
