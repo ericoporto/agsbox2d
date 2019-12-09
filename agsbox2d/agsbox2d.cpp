78c8033b09044e9569d1055492b58b3132583829
ericoporto <ericoporto2008@gmail.com>
2019-12-08 22:23:53 -0300

    upgrades version in the code.

--- agsbox2d/agsbox2d.cpp
+++ agsbox2d/agsbox2d.cpp
@@ -556,7 +556,7 @@ void AGS_EditorProperties(HWND parent)                        //*** optional ***
         // User has chosen to view the Properties of the plugin
         // We could load up an options dialog or something here instead
 	MessageBox(parent,
-             L"agsbox2d v0.4.0 By eri0o",
+             L"agsbox2d v0.5.0 By eri0o",
              L"About",
          MB_OK | MB_ICONINFORMATION);
 
@@ -2031,7 +2031,7 @@ int AGS_EngineOnEvent(int event, int data)                    //*** optional ***
         int screenWidth, screenHeight, colDepth;
         engine->GetScreenDimensions(&screenWidth, &screenHeight, &colDepth);
         debugDraw.InitializeAgsDebugDraw(engine, screenWidth, screenHeight, colDepth);
-        printf("\nagsbox2d 0.4.0\n");
+        printf("\nagsbox2d 0.5.0\n");
         do_only_once = true;
     }
   }
